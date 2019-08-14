00648f0939c4b135dd70402f56ed761a1a85ea56
adam116 <54105639+adam116@users.noreply.github.com>
2019-08-14 23:23:32 +0530

    Add files via upload

---
+++ 09/Pynkynmaw_Q2.c
@@ -0,0 +1,23 @@
+/* Write a program to input a positive number and reverse the digits of the number. */
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+  int n, rem, rev = 0;
+
+  printf("\nPlease Enter any number to Reverse\n");
+  scanf("%d", & n);
+
+  while (n > 0)
+   {
+     rem = n % 10;
+     rev = rev *10+ rem;
+     n = n /10;
+   }
+
+  printf("The reverse of a number is = %d\n", rev);
+
+  getch();
+}
---
+++ 09/Pynkynmaw_Q4.c
@@ -0,0 +1,29 @@
+/*  Write a program to generate all the prime numbers till N. */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j,n;
+
+    printf("Enter the number of terms (limit) \n");
+    scanf("%d",&n);
+
+    printf("The Prime numbers are:\n");
+    for(i=2;i<=n;i++)
+    {
+        int c=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                c++;
+            }
+        }
+
+       if(c==2)
+        {
+            printf("%d ",i);
+        }
+    }
+    getch();
+}
