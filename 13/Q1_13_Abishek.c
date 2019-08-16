ee686eb0db860395f1d3c70d4348245422b626a1
AbishekBista <38556597+AbishekBista@users.noreply.github.com>
2019-08-16 20:17:08 +0530

    Update and rename Q1_13_Abishek.c to 13/Q1_13_Abishek.c

---
+++ 13/Q1_13_Abishek.c
@@ -0,0 +1,36 @@
+/*Q1. Write a program that determine if a number entered by the user at run time is an Armstrong number or not.
+
+//Algorithm:
+1) Start
+2) Declare variables n, temp, sum, r
+3) Read variable n and initialize sum to 0
+4) Set temp = n
+5) While n>0, performs steps 6 through 8
+6) Set r = n % 10
+7) Set sum = sum + r * r * r
+8) Set n = n / 10
+9) If temp = sum
+    Display temp is an Armstrong Number
+  Else
+    Display temp is not an Armstrong Number
+10) Stop
+*/
+
+#include<stdio.h>
+void main()
+{
+  int n, temp, sum, r;
+  sum = n;
+  printf("Enter a number:");
+  scanf("%d", &n);
+  temp=n;
+  while(n>0)
+  {
+    r = n % 10;
+    sum = sum + r * r * r;
+    n = n / 10;
+  }
+  if(temp == sum)
+    printf("%d is an Armstrong Number.");
+  else
+    printf("%d is not an Armstrong Number.");
