7aab974cc342bd3277bcf3192ee0d5c6b979faee
AbishekBista <38556597+AbishekBista@users.noreply.github.com>
2019-08-17 20:47:29 +0530

    Create Q5_13_Abishek.c

---
+++ 13/Q5_13_Abishek.c
@@ -0,0 +1,4 @@
+/* Q5. Write a program to input the roll no., marks in 3 subjects and marks in one additional subject for N number of students.
+Compute the total marks, excess additional marks should be added to the total marks and determine the division.
+No division should be given if fail in 1 or more subjects.
+*/
