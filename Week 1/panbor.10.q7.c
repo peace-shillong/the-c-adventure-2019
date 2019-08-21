bffc8e56091755afefc3679fd1342584b0f1db6c
panb0r <54111178+panb0r@users.noreply.github.com>
2019-08-21 12:49:12 +0000

    Create panbor.10.q7.c

---
+++ Week 1/panbor.10.q7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+void main ()
+void Add (int a,int b)
+{
+  Clrscr ();
+  int a,b,c;
+  printf ("enter the values a and b\n\n");
+  scanf ("%d%d", &a&b);
+  c=a+b;
+   printf ("sum is=",&c);
+  Getch ();
+
+}
+void Add (int a, int b)
+   {
+    int c;
+    c=a+b;
+    return c;
+   }
+}
