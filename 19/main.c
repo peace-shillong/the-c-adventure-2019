52aeaf505295bb4aaa7aa0ff66674445fc1c0cf8
Yona <54110231+yoona189@users.noreply.github.com>
2019-08-21 00:32:52 +0000

    Add files via upload

---
+++ 19/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,r,rev=0;
+    printf("Please enter any number :");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    printf("the reverse number is %d",rev);
+
+}
