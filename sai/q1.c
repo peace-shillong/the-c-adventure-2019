f8d59dd690542390916c285a2163014ce071d0de
kynsai06 <saistephan9@gmail.com>
2019-10-03 11:13:03 +0530

    Create q1.c

---
+++ sai/q1.c
@@ -0,0 +1,13 @@
+/* 	
+    Q1. Write a program that determine if a number entered by the user at run time is an Armstrong number or not.
+    Reference: Book; link; or any other  	
+    Algo: (in your own words) 	 	
+  */
+  #include<stdio.h> 	
+  #include<conio.h> 	
+  void main() 	
+  { 		
+    char no;		 		
+    printf("Enter Your Roll No");
+    scanf("%d",&no);
+ }
